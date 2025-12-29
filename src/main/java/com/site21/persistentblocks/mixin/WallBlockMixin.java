79a3884fa31a3ed79a62ab1de1e465e4abdfa39d
SmokeyBBQx <47431313+SmokeyBBQx@users.noreply.github.com>
2025-12-29 14:47:30 +0100

    Fixed wall crash

--- src/main/java/com/site21/persistentblocks/mixin/WallBlockMixin.java
+++ src/main/java/com/site21/persistentblocks/mixin/WallBlockMixin.java
@@ -1,5 +1,6 @@
 package com.site21.persistentblocks.mixin;
 
+import com.google.common.collect.ImmutableMap;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.core.BlockPos;
@@ -23,23 +24,40 @@ import java.util.Map;
 
 @Mixin(WallBlock.class)
 public class WallBlockMixin {
-//    @Inject(at = @At("HEAD"), method = "createBlockStateDefinition")
-//    protected void persistentblocks$createBlockStateDefinition(StateDefinition.@NotNull Builder<Block, BlockState> builder, CallbackInfo ci) {
-//        builder.add(BlockStateProperties.PERSISTENT);
-//    }
+    @Inject(at = @At("HEAD"), method = "createBlockStateDefinition")
+    protected void persistentblocks$createBlockStateDefinition(StateDefinition.@NotNull Builder<Block, BlockState> builder, CallbackInfo ci) {
+        builder.add(BlockStateProperties.PERSISTENT);
+    }
 
-//
-//    @Inject(at = @At("RETURN"), method = "updateShape(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/Direction;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/LevelAccessor;Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;", cancellable = true)
-//    protected void updateShape(@NotNull BlockState state, Direction facing, BlockState facingState, @NotNull LevelAccessor level, BlockPos currentPos, BlockPos facingPos, CallbackInfoReturnable<BlockState> cir) {
-//        if (state.hasProperty(BlockStateProperties.PERSISTENT) && state.getValue(BlockStateProperties.PERSISTENT)) {
-//            cir.setReturnValue(state);
-//        }
-//    }
-//
-//    @Inject(at = @At("HEAD"), method = "updateShape(Lnet/minecraft/world/level/LevelReader;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;ZZZZ)Lnet/minecraft/world/level/block/state/BlockState;", cancellable = true)
-//    protected void persistentblocks$updateShape(LevelReader level, @NotNull BlockState state, BlockPos pos, BlockState neighbour, boolean northConnection, boolean eastConnection, boolean southConnection, boolean westConnection, CallbackInfoReturnable<BlockState> cir) {
-//        if (state.getValue(BlockStateProperties.PERSISTENT)) {
-//            cir.setReturnValue(state);
-//        }
-//    }
+
+    @Inject(at = @At("RETURN"), method = "updateShape(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/Direction;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/LevelAccessor;Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;", cancellable = true)
+    protected void updateShape(@NotNull BlockState state, Direction facing, BlockState facingState, @NotNull LevelAccessor level, BlockPos currentPos, BlockPos facingPos, CallbackInfoReturnable<BlockState> cir) {
+        if (state.hasProperty(BlockStateProperties.PERSISTENT) && state.getValue(BlockStateProperties.PERSISTENT)) {
+            cir.setReturnValue(state);
+        }
+    }
+
+    @Inject(at = @At("HEAD"), method = "updateShape(Lnet/minecraft/world/level/LevelReader;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;ZZZZ)Lnet/minecraft/world/level/block/state/BlockState;", cancellable = true)
+    protected void persistentblocks$updateShape(LevelReader level, @NotNull BlockState state, BlockPos pos, BlockState neighbour, boolean northConnection, boolean eastConnection, boolean southConnection, boolean westConnection, CallbackInfoReturnable<BlockState> cir) {
+        if (state.getValue(BlockStateProperties.PERSISTENT)) {
+            cir.setReturnValue(state);
+        }
+    }
+
+    @Inject(method = "makeShapes", at = @At("RETURN"), cancellable = true)
+    private void onMakeShapes(float width, float depth, float wallPostHeight, float wallMinY,
+                              float wallLowHeight, float wallTallHeight, @NotNull CallbackInfoReturnable<Map<BlockState, VoxelShape>> cir) {
+        Map<BlockState, VoxelShape> originalMap = cir.getReturnValue();
+        ImmutableMap.Builder<BlockState, VoxelShape> builder = ImmutableMap.builder();
+
+        for (Map.Entry<BlockState, VoxelShape> entry : originalMap.entrySet()) {
+            BlockState state = entry.getKey();
+            VoxelShape shape = entry.getValue();
+
+            builder.put(state.setValue(BlockStateProperties.PERSISTENT, Boolean.FALSE), shape);
+            builder.put(state.setValue(BlockStateProperties.PERSISTENT, Boolean.TRUE), shape);
+        }
+
+        cir.setReturnValue(builder.build());
+    }
 }
